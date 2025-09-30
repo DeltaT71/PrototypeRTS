0b73d7532853dcb442c6c79830c6c95431e063e6
DeltaT71 <130205202+DeltaT71@users.noreply.github.com>
2025-09-30 18:17:14 +0300

    Camera movement: Implemented camera movement

---
+++ Assets/Scripts/UI Scripts/CameraMovement.cs
@@ -0,0 +1,150 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("General Settings")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float cameraMovementSmoothing;
+    [SerializeField] private float cameraRotationSmoothing;
+    [SerializeField] private float mouseEdgeMovementThreshhold;
+
+    [Header("Toggle Movement Systems")]
+    [SerializeField] private bool toggleArrowKeysMovement;
+    [SerializeField] private bool toggleWASDKeysMovement;
+    [SerializeField] private bool toggleMouseEdgeMovement;
+    [SerializeField] private bool toggleCameraDragMovement;
+    [SerializeField] private bool toggleCameraRotation;
+
+
+    private Vector3 newPosition;
+    private Vector3 startingMouseDragPosition;
+    private Vector3 currentMouseDragPosition;
+    private Quaternion newRotation;
+
+    void Awake()
+    {
+        newPosition = transform.position;
+        newRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (toggleMouseEdgeMovement) HandleMouseEdgeMovement();
+        if (toggleArrowKeysMovement) HandleArrowKeysMovement();
+        if (toggleWASDKeysMovement) HandleWASDKeysMovement();
+        if (toggleCameraRotation) HandleCameraRotation();
+        if (toggleCameraDragMovement) HandleMouseDragMovement();
+
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, newPosition, Time.deltaTime * cameraMovementSmoothing);
+        cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, newRotation, Time.deltaTime * cameraRotationSmoothing);
+    }
+
+    private void HandleMouseEdgeMovement()
+    {
+        if (Input.mousePosition.x > Screen.width - mouseEdgeMovementThreshhold)
+        {
+            newPosition += cameraTransform.right * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x < mouseEdgeMovementThreshhold)
+        {
+            newPosition += cameraTransform.right * -cameraSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y > Screen.height - mouseEdgeMovementThreshhold)
+        {
+            newPosition += cameraTransform.forward * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y < mouseEdgeMovementThreshhold)
+        {
+            newPosition += cameraTransform.forward * -cameraSpeed * Time.deltaTime;
+        }
+    }
+
+    private void HandleArrowKeysMovement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += cameraTransform.right * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += cameraTransform.right * -cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += cameraTransform.forward * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += cameraTransform.forward * -cameraSpeed * Time.deltaTime;
+        }
+    }
+    private void HandleWASDKeysMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPosition += cameraTransform.right * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            newPosition += cameraTransform.right * -cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            newPosition += cameraTransform.forward * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            newPosition += cameraTransform.forward * -cameraSpeed * Time.deltaTime;
+        }
+    }
+
+    private void HandleCameraRotation()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            newRotation = Quaternion.Euler(new Vector3(0, -90, 0));
+        }
+        else if (newRotation != Quaternion.Euler(new Vector3(0, -45, 0)) && !Input.GetKey(KeyCode.E) && !Input.GetKey(KeyCode.Q))
+        {
+            newRotation = Quaternion.Euler(new Vector3(0, -45, 0));
+        }
+    }
+
+    private void HandleMouseDragMovement()
+    {
+        if (Input.GetMouseButtonDown(2) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                startingMouseDragPosition = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(2) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                currentMouseDragPosition = ray.GetPoint(entry);
+
+                newPosition = transform.position + startingMouseDragPosition - currentMouseDragPosition;
+            }
+        }
+    }
+}
